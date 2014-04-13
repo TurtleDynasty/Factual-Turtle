318b5e8ecf6cba47c537767ce210e1cff96261b9
TurtleDynasty <TurtleDynasty@users.noreply.github.com>
2014-04-12 17:36:23 -0700

    Update Tweeter
    Allow for string argument passing and return success

--- Tweeter.py
+++ Tweeter.py
@@ -4,19 +4,27 @@ import simplejson
 import twitter
 import Sentence
 
-api = twitter.Api(consumer_key='5T8HEXRlLhxcnMU5RhC8UcX4y',
+def buildTweet(phrase):
+    if phrase[0] == '@' and phrase[1] != '@':
+    
+        api = twitter.Api(consumer_key='5T8HEXRlLhxcnMU5RhC8UcX4y',
                   consumer_secret='QA5CIz4USVz0SuftfBXXyMwI6xHBnSrfxiDEAVrZFJi6qLUqa2',
                   access_token_key='2415264698-dg4OyNh4m9lxoSVjw6g9cqOroQi3lx22KvXm5n2',
                   access_token_secret='f1CH79PDiYANKNVCVcEBYeRKJTuef65nN3LqDlGyLtiid')
 
-tweet = Sentence.makeSentence('pattern.txt')
+        tweet = phrase + Sentence.makeSentence('pattern.txt')
 
-print("Tweet the following sentence?")
-print(tweet)
-ans = raw_input("(Y/N): ")
+        #print("Tweet the following sentence?")
+        #print(tweet)
+        #ans = raw_input("(Y/N): ")
 
-if (ans == "Y" or ans == "y"):
-    print("Tweet sent!")
-    status = api.PostUpdate(tweet)
-else:
-    print("Tweet not sent.")
+        #if (ans == "Y" or ans == "y"):
+            #print("Tweet sent!")
+            #status = api.PostUpdate(tweet)
+        #else:
+            #print("Tweet not sent.")
+            
+        status = api.PostUpdate(tweet)
+        return true
+    else:
+        return false
